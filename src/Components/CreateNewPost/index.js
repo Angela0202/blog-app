74d059a0b018e00baecfdb88dfd0cda3d48d14b2
Angela <ang.hayrapetian@gmail.com>
2018-07-11 16:45:19 +0400

    [add] create post form and adding posts to local storage

---
+++ src/Components/CreateNewPost/index.js
@@ -0,0 +1,138 @@
+import React, { Component } from 'react';
+
+import styles from './styles';
+import { withStyles } from '@material-ui/core/styles';
+import { TextField, Button } from '@material-ui/core/';
+import SaveIcon from '@material-ui/icons/Save';
+
+import { Redirect } from 'react-router-dom';
+
+class CreateNewPost extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      post: {
+        title: '',
+        body: '',
+        image: '',
+        authorID: this.props.currentUser.userID,
+        authorName: this.props.currentUser.username,
+        date: this.getDate()
+      },
+
+      redirect: false
+    };
+  }
+
+  static ID = Object.keys(localStorage).length;
+
+  getDate() {
+    let currentDate = new Date(),
+      day = currentDate.getDate(),
+      month = currentDate.getMonth() + 1,
+      year = currentDate.getFullYear();
+
+    if (day < 10) {
+      day = '0' + day;
+    }
+
+    if (month < 10) {
+      month = '0' + month;
+    }
+
+    currentDate = month + '/' + day + '/' + year;
+
+    return currentDate;
+  }
+
+  onChange = name => event => {
+    this.setState({
+      post: {
+        ...this.state.post,
+        [name]: event.target.value
+      }
+    });
+  };
+
+  onClick = () => {
+    const { title, body } = this.state.post;
+    console.log(CreateNewPost.ID);
+
+    if (title.trim() !== '' && body.trim() !== '') {
+      const post = { ...this.state.post, postID: ++CreateNewPost.ID };
+      localStorage.setItem(post.postID, JSON.stringify(post));
+
+      this.setState(
+        {
+          post: {
+            title: '',
+            body: '',
+            image: '',
+            authorID: this.props.currentUser.userID,
+            authorName: this.props.currentUser.username,
+            date: this.getDate()
+          },
+          redirect: true
+        }/*,
+        () => {
+          if (this.state.redirect) {
+            window.location.href = '/';
+            /!*return <Redirect to="/" />*!/
+          }
+        }*/
+      );
+    }
+  };
+
+  render() {
+    const { classes } = this.props;
+    const { title, body, image, authorID } = this.state.post;
+    console.log(authorID);
+    return (
+      <div className={classes.container}>
+        <h1> Add New Post </h1>
+        <form autoComplete="off" className={classes.formContainer}>
+          <TextField
+            id="title"
+            label="Title"
+            className={classes.textField}
+            value={title}
+            onChange={this.onChange('title')}
+            margin="normal"
+          />
+          <TextField
+            id="multiline-static"
+            rows="10"
+            label="Body"
+            multiline
+            rowsMax="10"
+            value={body}
+            onChange={this.onChange('body')}
+            className={classes.textField}
+            margin="normal"
+          />
+          <TextField
+            id="name"
+            label="Featured Image URL"
+            className={classes.textField}
+            value={image}
+            onChange={this.onChange('image')}
+            margin="normal"
+          />
+          <Button
+            variant="contained"
+            size="small"
+            className={classes.button}
+            onClick={this.onClick}
+          >
+            <SaveIcon />
+            Save
+          </Button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default withStyles(styles)(CreateNewPost);
---
+++ src/Components/CreateNewPost/styles.js
@@ -0,0 +1,20 @@
+export default {
+  container: {
+    display: 'flex',
+    flexDirection: 'column',
+    width: '100vw',
+    alignItems: 'center',
+    padding: '70px 50px'
+  },
+
+  formContainer: {
+    display: 'flex',
+    flexDirection: 'column',
+    maxWidth: '800px',
+    justifyContent: 'center'
+  },
+
+  textField: {
+    width: 500,
+  },
+}
