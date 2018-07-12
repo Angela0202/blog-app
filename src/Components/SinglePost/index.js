b4b74e72b642dc9f03d74b110be9c0745dece226
Angela <ang.hayrapetian@gmail.com>
2018-07-12 14:30:34 +0400

    [add] single post editing

--- src/Components/SinglePost/index.js
+++ src/Components/SinglePost/index.js
@@ -9,11 +9,14 @@ import {
   CardMedia,
   Typography,
   Button,
-  CardActions
+  CardActions,
+  TextField
 } from '@material-ui/core/';
 import FavoriteIcon from '@material-ui/icons/Favorite';
 import CommentList from '../CommentList';
 import CreateComment from '../CreateComment';
+import EditIcon from '@material-ui/icons/Edit';
+import SaveIcon from '@material-ui/icons/Save';
 
 class SinglePost extends Component {
   constructor(props) {
@@ -21,11 +24,13 @@ class SinglePost extends Component {
 
     this.state = {
       clicked: 0,
-      comments: []
-    };
+      comments: [],
+      isEditing: false,
+      value: JSON.parse(SinglePost.getSingleItemFromLocalStorage(SinglePost.getSinglePostID())).body
+    }
   }
 
-  onCommentCreate = (comment) => {
+  onCommentCreate = comment => {
     const { comments } = this.state;
 
     const postID = SinglePost.getSinglePostID(),
@@ -33,12 +38,18 @@ class SinglePost extends Component {
       postInJsonFormat = JSON.parse(post);
 
     this.setState({
-      comments: [{...comment}, ...comments]
+      comments: [{ ...comment }, ...comments]
     });
 
-    postInJsonFormat.comments = postInJsonFormat.comments.concat([comment, ...comments]);
+    postInJsonFormat.comments = postInJsonFormat.comments.concat([
+      comment,
+      ...comments
+    ]);
 
-    localStorage.setItem(postInJsonFormat.postID, JSON.stringify({...postInJsonFormat}));
+    localStorage.setItem(
+      postInJsonFormat.postID,
+      JSON.stringify({ ...postInJsonFormat })
+    );
   };
 
   static getSinglePostID() {
@@ -58,12 +69,33 @@ class SinglePost extends Component {
     }));
   };
 
+  onEditOrSaveButtonClick = (e) => {
+    console.log(e.target);
+    const { isEditing } = this.state;
+    !isEditing ?
+      this.setState({
+        isEditing: true
+      })
+      :
+      this.setState({
+        isEditing: false,
+        value: this.state.value
+      });
+  };
+
+  onChange = (e) => {
+    this.setState({
+      value: e.target.value
+    })
+  };
+
   render() {
     const postID = SinglePost.getSinglePostID(),
       post = SinglePost.getSingleItemFromLocalStorage(postID),
       postInJsonFormat = JSON.parse(post);
 
     const { classes } = this.props;
+    const { isEditing, value } = this.state;
 
     return (
       <div>
@@ -76,16 +108,38 @@ class SinglePost extends Component {
           />
           <CardMedia className={classes.media} image={postInJsonFormat.image} />
           <CardContent>
-            <Typography component="p">{postInJsonFormat.body}</Typography>
+            {!isEditing ? (
+              <Typography component="p">{value}</Typography>
+            ) : (
+              <TextField
+                id="multiline-static"
+                label="Multiline"
+                multiline
+                rows="4"
+                value={value}
+                className={classes.textField}
+                margin="normal"
+                fullWidth
+                onChange={this.onChange}
+              />
+            )}
           </CardContent>
           <CardActions className={classes.actions} disableActionSpacing>
             <Button aria-label="Add to favorites" onClick={this.onClick}>
               <FavoriteIcon />
               <span>{this.state.clicked}</span>
             </Button>
+            <Button
+              variant="fab"
+              color="secondary"
+              className={classes.button}
+              onClick={this.onEditOrSaveButtonClick}
+            >
+              {!isEditing ? (<EditIcon />) : (<SaveIcon />)}
+            </Button>
           </CardActions>
-          <CommentList comments={postInJsonFormat.comments}/>
-          <CreateComment onCommentCreate={this.onCommentCreate}/>
+          <CommentList comments={postInJsonFormat.comments} />
+          <CreateComment onCommentCreate={this.onCommentCreate} />
         </Card>
       </div>
     );
