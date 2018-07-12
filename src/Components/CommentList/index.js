e9ae3d9fa05fca965c2c7b034f7bf8987a53f85a
Angela <ang.hayrapetian@gmail.com>
2018-07-12 08:51:04 +0400

    [add] drawing comment list

--- src/Components/CommentList/index.js
+++ src/Components/CommentList/index.js
@@ -2,20 +2,35 @@ import React, { Component } from 'react';
 
 import styles from './styles';
 import { withStyles } from '@material-ui/core/styles';
-import { TextField, Button } from '@material-ui/core/';
+import {
+  Grid,
+  Typography,
+  List,
+  ListItem,
+  ListItemText
+} from '@material-ui/core/';
 
 class CommentList extends Component {
   render() {
     const { comments } = this.props;
 
-    return(
-      <ul>
+    return (
+      <Grid container spacing={16}>
         {
           comments.map((comment, index) => (
-            <li key={index}>{comment.body}</li>
+            <Grid item xs={12} md={6} key={index}>
+              <Typography variant="title">{comment.title}</Typography>
+              <div>
+                <List>
+                  <ListItem>
+                    <ListItemText primary={comment.commentBody} />
+                  </ListItem>
+                </List>
+              </div>
+            </Grid>
           ))
         }
-      </ul>
+      </Grid>
     );
   }
 }
