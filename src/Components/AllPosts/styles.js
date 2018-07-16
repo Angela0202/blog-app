fd5dd40856379486471cb96b7da0f0955c6d4df7
Angela <ang.hayrapetian@gmail.com>
2018-07-16 12:57:42 +0400

    [add] boxShadow to post card

--- src/Components/AllPosts/styles.js
+++ src/Components/AllPosts/styles.js
@@ -8,11 +8,12 @@ export default {
     display: 'flex',
     flexDirection: 'column',
     justifyContent: 'space-between',
+    boxShadow: '0px 1px 3px 0px rgba(0, 0, 0, 0.2), 0px 1px 1px 0px rgba(0, 0, 0, 0.14), 0px 2px 1px -1px #57c9c4',
 
     '&:hover': {
       backgroundColor: '#57c9c4',
       transition: '.9s',
-      color: '#fff'
+      color: '#fff',
     }
   },
 
